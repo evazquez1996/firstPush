df93e11e888ec7e4e9b1bc5b197b005582f9644f
evazquez1996 <evazquez@sea.yearup.org>
2022-06-07 14:20:14 -0700

    Conditionals

---
+++ ifLive.js
@@ -0,0 +1,38 @@
+// console.log("Hello, world!");
+
+// let randomNumber = Math.random();
+
+// console.log(randomNumber);
+
+// if (randomNumber < 0.5) {
+//     console.log("Hello");
+//     console.log(randomNumber);
+// }
+
+// if (randomNumber >= 0.5) {
+//     console.log("Random Number is greater than 0.5");
+//     console.log(randomNumber);
+// }
+
+// let dayOfWeek = 'Saturday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log('This is Monday');
+// }else if(dayOfWeek === 'Thursday') {
+//     console.log('This is Thursday');
+// }else if(dayOfWeek === 'Saturday') {
+//     console.log('Hooray today is Saturday!');
+// }else {
+//     console.log('What day is it?');
+// }
+
+
+let age = prompt("What is your age?");
+
+if (age <= 15) {
+    console.log("You're to young to drive!");
+} else if (age >= 16) {
+    console.log("You are old enough to drive!");
+} else {
+    console.log("Ride the bus thaannn!");
+}
